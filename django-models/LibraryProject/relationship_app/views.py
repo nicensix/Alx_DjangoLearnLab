457272ae0a4f5e53a83acbc3f71d89a4fe1c4097
Emmanuel Godwin <nicensix@gmail.com>
2025-07-31 09:18:42 +0100

    Added DtetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book
-from .models import Library  # <-- Required exact line for checker
+from .models import Library  
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
