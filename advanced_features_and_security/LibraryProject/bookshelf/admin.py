fe29e7df864a40e615c13be551257342b49d485e
Emmanuel Godwin <nicensix@gmail.com>
2025-08-24 09:41:19 +0100

    Custom permissions added in Book model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
+from django.contrib.auth.models import Group, Permission
+from django.apps import apps
+
+Book = apps.get_model('bookshelf', 'Book')
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -17,6 +21,23 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {"fields": ("date_of_birth", "profile_photo")}),
     )
+
+def create_groups():
+    # Get model permissions
+    can_view = Permission.objects.get(codename='can_view', content_type__app_label='bookshelf')
+    can_create = Permission.objects.get(codename='can_create', content_type__app_label='bookshelf')
+    can_edit = Permission.objects.get(codename='can_edit', content_type__app_label='bookshelf')
+    can_delete = Permission.objects.get(codename='can_delete', content_type__app_label='bookshelf')
+
+    # Create groups
+    viewers, _ = Group.objects.get_or_create(name="Viewers")
+    editors, _ = Group.objects.get_or_create(name="Editors")
+    admins, _ = Group.objects.get_or_create(name="Admins")
+
+    # Assign permissions
+    viewers.permissions.set([can_view])
+    editors.permissions.set([can_view, can_create, can_edit])
+    admins.permissions.set([can_view, can_create, can_edit, can_delete])
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book)
