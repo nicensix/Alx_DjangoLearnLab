d1db61ef906ce75e2248dfcb314fbbe5f4936b2e
Emmanuel Godwin <nicensix@gmail.com>
2025-08-27 23:51:42 +0100

    Task 3: Added authentication and permissions with DRF tokens

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # third-party
     'rest_framework',
+    'rest_framework.authtoken',
 
     # local apps
     'api',
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # secure by default
+    ],
+}
